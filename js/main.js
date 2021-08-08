670802396d71de5486810db7102cfb6a3ba4d900
dezoliveira <andresoliveira@protonmail.com>
2021-08-07 23:28:19 -0300

    Adicionado a section  Home + Sticky

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 //cancelBtn => Javascript para fechar o menu
 //navBar => Javascript para ativar e desativar a navbar mobile
 
+// Sidebar
 let menuBtn = document.querySelector(".menu-btn");
 let cancelBtn = document.querySelector(".cancel-btn");
 let navBar = document.querySelector(".navbar");
@@ -16,4 +17,15 @@ cancelBtn.onclick = function(){
     menuBtn.style.opacity = "1";
     menuBtn.style.pointerEvents = "auto";
     navBar.classList.remove("active")
+}
+
+//Sticky Nav Menu
+let nav = document.querySelector("nav")
+let val
+window.onscroll = function(){
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("sticky")
+    }else{
+        nav.classList.remove("sticky")
+    }
 }
